a42b668244aadc5271964c09dc3b834b0ea8856a
Nadi <kitti-46@ya.ru>
2023-10-08 21:33:55 +0300

    rename test

--- test_taskHome.py
+++ test_taskHome.py
@@ -35,22 +35,17 @@ import subprocess
 import pytest
 
 
-def check_text(cmd, text):
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+def check_text(command, text):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
     if text in result.stdout and result.returncode == 0:
         return True
     else:
         return False
 def test_testing1():
-    assert check_text("cat /etc/os-release", "jammy"), "test1 FAIL"
+    assert check_text("cat /etc/os-release", "Jammyy"), "test1 FAIL"
 
 #@pytest.mark.run_this
 def test_testing2():
     assert check_text("cat /etc/os-release", "22.04.1"), "test2 FAIL"
 
-
-def test_testing3():
-    assert check_text("cat /etc/os-release", "NAME"), "test3 FAIL"
-
-# запуск из терминала: pytest test_taskHome.py
-# или с ключом -v для более информативного вывода: pytest test_taskHome.py -v
+ # pytest test_taskHome.py -v
